126eea79e39cb9a86a8f7df38b108d61dc609cf4
Brecht Hoflack <brh@melexis.com>
2014-12-10 15:13:06 +0100

    Use etcd to register and get the other nodes' addresses

--- message-test/src/message_test/test.clj
+++ message-test/src/message_test/test.clj
@@ -39,11 +39,11 @@
                                         ; Signal that the listener is ready
             (close! init-chan)
 
-            ; Block till the chan is quit
+                                        ; Block till the chan is quit
             (while (not (nil? (<!! kill-chan)))
               (Thread/sleep 100)))))))
     
-    ; Block till initialization is ready
+                                        ; Block till initialization is ready
     (<!! init-chan)
     
     kill-chan))
@@ -53,7 +53,7 @@
   (let [messages (map (fn [_] (agent [])) uris)
         uri-messages (zipmap uris messages)]
 
-    ; Start a listener for all uris
+                                        ; Start a listener for all uris
     (let [kill-chans (-> (for [[uri messages] uri-messages] 
                            (listener-thread uri messages))
                          (doall))]
@@ -61,12 +61,13 @@
                                         ; Send the requested amount of messages
       (-> (for [i (range amount)]
             (let [n (* (count uris) (Math/random))
-                  uri (nth uris n)]
-              (println "Sending to uri" uri)            
+                  uri (nth uris n)
+                  ;uri (nth uris 2)
+                  ]
               (a/with-connection c uri
                 (a/with-session s c false :auto_acknowledge
                   (let [msg (a/create-message s (str (int i)))]
-
+                    (println "Sending message" i "to uri" uri)
                     (a/send s "topic" msg :topic))))))
           (doall))
 
@@ -84,12 +85,12 @@
          (if (not (every? (fn [[_ msgs]] (= amount (count @msgs)))
                           uri-messages))
            (do
-             (Thread/sleep (* 1000 n))
+             (Thread/sleep 1000)
              (recur (inc n))))))
 
-      (-> kill-chans
-          (map (fn [c] (close! c)))
-          (doall)))))
+      (->> kill-chans
+           (map (fn [c] (close! c)))
+           (doall)))))
 
-
-
+(defn test-servers-test []
+  (send-to-multiple-brokers 10 "failover:(tcp://esb-a-test.sensors.elex.be:61602,tcp://esb-b-test.sensors.elex.be:61602)" "failover:(tcp://esb-a-test.sofia.elex.be:61602,tcp://esb-b-test.sofia.elex.be:61602)" "failover:(tcp://esb-a-test.erfurt.elex.be:61602,tcp://esb-b-test.erfurt.elex.be:61602)" "failover:(tcp://esb-a-test.colo.elex.be:61602,tcp://esb-b-test.colo.elex.be:61602)" "failover:(tcp://esb-a-test.kuching.elex.be:61602,tcp://esb-b-test.kuching.elex.be:61602)"))
--- site-c/start.sh
+++ site-c/start.sh
@@ -1,7 +1,38 @@
 #!/bin/dash
 
-SITEA_NETWORK_CONNECTOR="static:($NETWORKOFBROKERSTEST_SITEA_1_PORT_61616_TCP)"
-SITEB_NETWORK_CONNECTOR="static:($NETWORKOFBROKERSTEST_SITEB_1_PORT_61616_TCP)"
-SITEC_NETWORK_CONNECTOR="static:($NETWORKOFBROKERSTEST_SITEC_1_PORT_61616_TCP)"
+# Publish my ip to etcd
+
+echo "Waiting for etcd to init"
+
+sleep 5
+
+IP=`hostname  -I | cut -f1 -d' '`
+HOSTNAME=`hostname`
+URL="http://172.17.42.1:4001/v2/keys/ip/$HOSTNAME"
+ADDRESS="static:(tcp://$IP:61616)"
+
+echo "Using etcd url $URL"
+echo "Setting $URL to to $ADDRESS"
+curl -XPUT $URL -d value="$ADDRESS"
+
+sleep 2
+
+echo "SITEA: `curl http://172.17.42.1:4001/v2/keys/ip/site-a` SITEB: `curl http://172.17.42.1:4001/v2/keys/ip/site-b` SITEC: `curl http://172.17.42.1:4001/v2/keys/ip/site-c`"
+
+SITEA_NETWORK_CONNECTOR=`curl http://172.17.42.1:4001/v2/keys/ip/site-a | perl -ne 'if (/.*"value":"([()\w\d.:\/]+)".*/) {print $1}'`
+SITEB_NETWORK_CONNECTOR=`curl http://172.17.42.1:4001/v2/keys/ip/site-b | perl -ne 'if (/.*"value":"([()\w\d.:\/]+)".*/) {print $1}'`
+SITEC_NETWORK_CONNECTOR=`curl http://172.17.42.1:4001/v2/keys/ip/site-c | perl -ne 'if (/.*"value":"([()\w\d.:\/]+)".*/) {print $1}'`
+
+echo "Setting SITEA_NETWORK_CONNECTOR to $SITEA_NETWORK_CONNECTOR"
+echo "Setting SITEB_NETWORK_CONNECTOR to $SITEB_NETWORK_CONNECTOR"
+echo "Setting SITEC_NETWORK_CONNECTOR to $SITEC_NETWORK_CONNECTOR"
+
+MY_ENV=`env`
+
+echo "My ENV: $MY_ENV"
+
+export SITEA_NETWORK_CONNECTOR
+export SITEB_NETWORK_CONNECTOR
+export SITEC_NETWORK_CONNECTOR
 
 java -Xms1G -Xmx1G -Djava.util.logging.config.file=logging.properties -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote -Djava.io.tmpdir=/tmp -Dactivemq.classpath=apache-activemq-5.5.1/conf -Dactivemq.home=apache-activemq-5.5.1 -Dactivemq.base=apache-activemq-5.5.1 -Dactivemq.conf=apache-activemq-5.5.1/conf -Dactivemq.data=apache-activemq-5.5.1/data -jar apache-activemq-5.5.1/bin/run.jar start
