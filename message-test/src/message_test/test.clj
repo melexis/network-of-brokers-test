253aef63cdb2570061f44b0fb41e5189eb4987f5
Lien Deboosere <leb@melexis.com>
2014-12-22 11:27:09 +0100

    make the test work on esb nodes

--- message-test/src/message_test/test.clj
+++ message-test/src/message_test/test.clj
@@ -102,8 +102,41 @@
         (close! killed-chan))))
     [kill-chan killed-chan endpoints]))
 
+(defn ^:private send-messages [amount uris]
+                                        ; Send the requested amount of messages
+  (-> (for [i (range amount)]
+        (let [n (* (count uris) (Math/random))
+              uri (nth uris n)]
+          (a/with-connection c uri
+            (a/with-session s c false :auto_acknowledge
+              (let [msg (a/create-message s (str (int i)))]
+                (println "Sending message" i "to uri" uri)
+                (a/send s "VirtualTopic.topic" msg :topic))))))
+      (doall)))
+
+(defn ^:private receive-messages [amount uri-messages]
+  (doall 
+   (loop [n 0]
+     
+     (println "After" n "seconds:")
+     (-> (for [[uri msgs] uri-messages]
+           (do
+             (println "Uri" uri "messages" (count @msgs))
+;             (println "Endpoints:" @interruption-endpoints)
+             (println "Duplicates:" (->> @msgs
+                                         (group-by identity)
+                                         (filter #(> (count (second %)) 1))
+                                         (map first)))))
+         (doall))
+
+     (if (not (every? (fn [[_ msgs]] (= amount (count @msgs)))
+                      uri-messages))
+       (do
+         (Thread/sleep 1000)
+         (recur (inc n)))))))
+
 (defn send-to-multiple-brokers
-  [amount & uris]
+  [amount {:keys [interrupt?] :or {interrupt? true}} & uris]
   (let [messages (map (fn [_] (agent [])) uris)
         uri-messages (zipmap uris messages)]
 
@@ -112,55 +145,40 @@
                                 (let [destination-name (str "Consumer." (.toString (java.util.UUID/randomUUID)) ".VirtualTopic.topic")]
                                   (listener-thread uri messages destination-name :queue))) 
                               uri-messages)                     
-                         (doall))
+                         (doall))]
+
+      (if interrupt?
                                         ; Start the interruption threads that will randomly drop messages
-          [interruption-kill-chan interruption-killed-chan interruption-endpoints] (interruption-thread 20000)]
-
-                                        ; Send the requested amount of messages
-      (-> (for [i (range amount)]
-            (let [n (* (count uris) (Math/random))
-                  uri (nth uris n)
-                  ;uri (nth uris 2)
-                  ]
-              (a/with-connection c uri
-                (a/with-session s c false :auto_acknowledge
-                  (let [msg (a/create-message s (str (int i)))]
-                    (println "Sending message" i "to uri" uri)
-                    (a/send s "VirtualTopic.topic" msg :topic))))))
-          (doall))
-
+        (let [[interruption-kill-chan interruption-killed-chan interruption-endpoints] (interruption-thread 20000)]
+          (send-messages amount uris)
                                         ; Sending is completed,  signal the interruption thread to stop
-      (close! interruption-kill-chan)
-      (println "Waiting for the interruption kill chan to stop")
-      (<!! interruption-killed-chan)
-
+          (close! interruption-kill-chan)
+          (println "Waiting for the interruption kill chan to stop")
+          (<!! interruption-killed-chan)
                                         ; Verify that all listeners got the same amount of messages
-      (doall 
-       (loop [n 0]
-         
-         (println "After" n "seconds:")
-         (-> (for [[uri msgs] uri-messages]
-               (do
-                 (println "Uri" uri "messages" (count @msgs))
-                 (println "Endpoints:" @interruption-endpoints)
-                 (println "Duplicates:" (->> @msgs
-                                             (group-by identity)
-                                             (filter #(> (count (second %)) 1))
-                                             (map first)))
-                 
-                 ;(println (map #(.getText %) @msgs))
-                 ))
-             (doall))
+          (receive-messages amount uri-messages))
 
-         (if (not (every? (fn [[_ msgs]] (= amount (count @msgs)))
-                          uri-messages))
-           (do
-             (Thread/sleep 1000)
-             (recur (inc n))))))
-
+        (doall (send-messages amount uris)
+            (receive-messages amount uri-messages)))
+      
+                                        ; Kill the receiver threads
       (->> kill-chans
            (map (fn [c] (close! c)))
            (doall)))))
 
+
+
 (defn test-servers-test []
-  (send-to-multiple-brokers 10 "failover:(tcp://esb-a-test.sensors.elex.be:61602,tcp://esb-b-test.sensors.elex.be:61602)" "failover:(tcp://esb-a-test.sofia.elex.be:61602,tcp://esb-b-test.sofia.elex.be:61602)" "failover:(tcp://esb-a-test.erfurt.elex.be:61602,tcp://esb-b-test.erfurt.elex.be:61602)" "failover:(tcp://esb-a-test.colo.elex.be:61602,tcp://esb-b-test.colo.elex.be:61602)" "failover:(tcp://esb-a-test.kuching.elex.be:61602,tcp://esb-b-test.kuching.elex.be:61602)"))
+  (send-to-multiple-brokers 10 {:interrupt? false} "failover:(tcp://esb-a-test.sensors.elex.be:61602,tcp://esb-b-test.sensors.elex.be:61602)" "failover:(tcp://esb-a-test.sofia.elex.be:61602,tcp://esb-b-test.sofia.elex.be:61602)" "failover:(tcp://esb-a-test.erfurt.elex.be:61602,tcp://esb-b-test.erfurt.elex.be:61602)" "failover:(tcp://esb-a-test.colo.elex.be:61602,tcp://esb-b-test.colo.elex.be:61602)" "failover:(tcp://esb-a-test.kuching.elex.be:61602,tcp://esb-b-test.kuching.elex.be:61602)"))
+
+(defn uat-servers-test []
+  (send-to-multiple-brokers 10 {:interrupt? false} "failover:(tcp://esb-a-uat.sensors.elex.be:61602,tcp://esb-b-uat.sensors.elex.be:61602)" "failover:(tcp://esb-a-uat.sofia.elex.be:61602,tcp://esb-b-uat.sofia.elex.be:61602)" "failover:(tcp://esb-a-uat.erfurt.elex.be:61602,tcp://esb-b-uat.erfurt.elex.be:61602)" "failover:(tcp://esb-a-uat.colo.elex.be:61602,tcp://esb-b-uat.colo.elex.be:61602)" "failover:(tcp://esb-a-uat.kuching.elex.be:61602,tcp://esb-b-uat.kuching.elex.be:61602)"))
+
+
+(defn prod-servers-test []
+  (send-to-multiple-brokers 10 {:interrupt? false} "failover:(tcp://esb-a.sensors.elex.be:61602,tcp://esb-b.sensors.elex.be:61602)" "failover:(tcp://esb-a.sofia.elex.be:61602,tcp://esb-b.sofia.elex.be:61602)" "failover:(tcp://esb-a.erfurt.elex.be:61602,tcp://esb-b.erfurt.elex.be:61602)" "failover:(tcp://esb-a.colo.elex.be:61602,tcp://esb-b.colo.elex.be:61602)" "failover:(tcp://esb-a.kuching.elex.be:61602,tcp://esb-b.kuching.elex.be:61602)"))
+
+
+
+
+
